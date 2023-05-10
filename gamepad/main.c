34bb1ec9bd3f7c9d8298b5c13e68499df4172f2a
Santino <santinoo.rn@gmail.com>
2023-05-10 12:53:32 -0300

    upgrades on gamepad

--- gamepad/main.c
+++ gamepad/main.c
@@ -1,11 +1,15 @@
+#include <stdint.h>
 #include "serial.h"
 #include "sleep.h"
 #include "adc.h"
-#include <stdint.h>
 
 #define INIT_DATA_DIRECTION 0x00
 #define INIT_PULL_UP 0x03
 #define HIGH_BTN_SHOOT 0x01
+
+#define ADC_MAX_VALUE 1024
+#define ADC_LEFT_MOVE (ADC_MAX_VALUE / 2) - 100
+#define ADC_RIGHT_MOVE (ADC_MAX_VALUE / 2) + 100
 
 volatile unsigned char *PIN_B = (unsigned char *)0x23;    // direccion de PIN_B
 volatile unsigned char *DDR_B = (unsigned char *)0x24;    // direccion de DDR_B
@@ -13,7 +17,8 @@ volatile unsigned char *PUERTO_B = (unsigned char *)0x25; // direccion de PORT_B
 
 int main()
 {
-    uint8_t input_btn, input_adc;
+    uint8_t input_btn;
+    uint32_t input_adc;
     (*DDR_B) = INIT_DATA_DIRECTION;
     (*PUERTO_B) = INIT_PULL_UP;
     serial_init();
@@ -27,13 +32,42 @@ int main()
         sleep_ms(20);
 
         input_adc = adc_get(0);
-        serial_put_int(input_adc, 4);
-        serial_put_string("\r\n");
-        sleep_ms(90);
-
+        // serial_put_int(input_adc, 4);
+        // serial_put_string("\r\n");
+        sleep_ms_times(50, 10);
         if (!(input_btn & HIGH_BTN_SHOOT))
         {
-            serial_put_char(0x20);
+            switch (input_adc)
+            {
+            case 0 ... ADC_LEFT_MOVE:
+                serial_put_string("izquierda disparando\n");
+                break;
+            case ADC_LEFT_MOVE + 1 ... ADC_RIGHT_MOVE - 1:
+                serial_put_string("centro disparando\n");
+                break;
+            case ADC_RIGHT_MOVE ... ADC_MAX_VALUE:
+                serial_put_string("derecha disparando\n");
+                break;
+            default:
+                break;
+            }
+        }
+        else
+        {
+            switch (input_adc)
+            {
+            case 0 ... ADC_LEFT_MOVE:
+                serial_put_string("izquierda sin disparar\n");
+                break;
+            case ADC_LEFT_MOVE + 1 ... ADC_RIGHT_MOVE - 1:
+                serial_put_string("centro sin disparar\n");
+                break;
+            case ADC_RIGHT_MOVE ... ADC_MAX_VALUE:
+                serial_put_string("derecha sin disparar\n");
+                break;
+            default:
+                break;
+            }
         }
     };
     return 0;
