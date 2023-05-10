009dae7b9512d61859da3814be776ba4ef4128aa
Santino <santinoo.rn@gmail.com>
2023-05-10 01:13:58 -0300

    gamepad modified

--- gamepad/main.c
+++ gamepad/main.c
@@ -1,11 +1,11 @@
 #include "serial.h"
 #include "sleep.h"
+#include "adc.h"
 #include <stdint.h>
 
 #define INIT_DATA_DIRECTION 0x00
 #define INIT_PULL_UP 0x03
-#define HIGH_BTN_Z 0x02
-#define HIGH_BTN_X 0x01
+#define HIGH_BTN_SHOOT 0x01
 
 volatile unsigned char *PIN_B = (unsigned char *)0x23;    // direccion de PIN_B
 volatile unsigned char *DDR_B = (unsigned char *)0x24;    // direccion de DDR_B
@@ -13,28 +13,27 @@ volatile unsigned char *PUERTO_B = (unsigned char *)0x25; // direccion de PORT_B
 
 int main()
 {
-    uint8_t input;
+    uint8_t input_btn, input_adc;
     (*DDR_B) = INIT_DATA_DIRECTION;
     (*PUERTO_B) = INIT_PULL_UP;
     serial_init();
-    sleep_ms_times(90,10);
-    serial_put_char(0x20);
+    adc_init();
+    sleep_ms_times(90, 10);
+
     /* inf loop */
     while (1)
     {
-
-        input = (*PIN_B);
+        input_btn = (*PIN_B);
         sleep_ms(20);
-        // serial_put_string("input:");
-        // serial_put_int(input, 2);
-        // serial_put_string("\n");
-        if (!(input & HIGH_BTN_Z))
+
+        input_adc = adc_get(0);
+        serial_put_int(input_adc, 4);
+        serial_put_string("\r\n");
+        sleep_ms(90);
+
+        if (!(input_btn & HIGH_BTN_SHOOT))
         {
-            serial_put_char('h');
-        }
-        if (!(input & HIGH_BTN_X))
-        {
-            serial_put_char('l');
+            serial_put_char(0x20);
         }
     };
     return 0;
--- gamepad/run.sh
+++ gamepad/run.sh
@@ -1,4 +1,5 @@
-stty -F /dev/ttyACM0 speed 9600 # o 2400 la que necesiten
-cat /dev/ttyACM0 | while read -n 1 tecla; do
+stty -F /dev/ttyACM0 speed 9600
+cat /dev/ttyACM0 | 
+while read -n 1 tecla; do
     xdotool key $tecla
 done
