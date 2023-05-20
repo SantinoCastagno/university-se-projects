b788658e8fa75e88973167d0c0e8c90078f68551
Santino Castagno <santinocastagno.dev@gmail.com>
2023-05-20 05:09:22 -0300

    updates on timer1 driver

--- timer1_led/main.c
+++ timer1_led/main.c
@@ -1,19 +1,21 @@
 #include <avr/interrupt.h>
 
 #include "timer1.h"
-volatile unsigned char *DDR_B = (unsigned char *)0x24;	  // direccion de DDR_B
+volatile unsigned char *DDR_B = (unsigned char *)0x24; // direccion de DDR_B
 
 int main(void)
 {
-    (*DDR_B) = 0b00000111;	  // setear direccion de datos
-    timer1_init(1);
-    // leds_init();
-    // tasks_init();
-    // tasks_def(5000, toggle13); // definimos una tarea periódica a 300ms
-    // sei();                     // habilitamos las interrupciones
-    while (1)
-    {  
-        ;;
-    }
-    return 0;
+	(*DDR_B) |= 0b00000010; // setear direccion de datos
+	timer1_init(1);
+	while (1)
+	{
+		;
+		;
+	}
+
+	for (;;)
+	{
+		;
+	}
+	return 0;
 }
--- timer1_led/timer1.c
+++ timer1_led/timer1.c
@@ -9,10 +9,11 @@
 #include <stdint.h> // para los tipos de datos. Ej.: uint8_t
 #include <avr/interrupt.h>
 #include "timer1.h"
+#include "sleep.h"
 
 /* Macros para la configuracion de los registros de control */
-#define CONF_CONTROL_REG_A_FPWM 0b10100010; // fast PWM
-#define CONF_CONTROL_REG_B_FPWM 0b00011011; // fast PWM - CS02|CS01|CS00 (preescalar de 64)
+#define CONF_CONTROL_REG_A_FPWM 0b10000010; // fast PWM
+#define CONF_CONTROL_REG_B_FPWM 0b00011010; // fast PWM - CS02|CS01|CS00 (preescalar de 8)
 #define CONF_CONTROL_REG_C_FPWM 0b00000000; //
 #define CONF_CONTROL_REG_A_CTC 0b00000000;  // CTC
 #define CONF_CONTROL_REG_B_CTC 0b00011011;  // CTC - CS02|CS01|CS00 (preescalar de 64)
@@ -23,17 +24,26 @@
 
 /* Macros de valores */
 #define SYSTEM_TICKS 16000000
-#define PRESCALAR 64
+#define PRESCALAR 8
 #define TICKS_PER_SECOND 1000
-#define TIMER0_OCR1A ((SYSTEM_TICKS / PRESCALAR) / TICKS_PER_SECONDS)
-#define TIMER0_0CR1AH 0x13     // 250000 * 0.02s = 5000 = 0x1388
-#define TIMER0_0CR1AL 0x88     //
-#define TIMER0_0CR1BH_MIN 0x00 // 250000 * 0.001s = 250 = 0xfa
-#define TIMER0_0CR1BL_MIN 0xfa //
-#define TIMER0_0CR1BH_MID 0x01 // 375 = 0x1f4
-#define TIMER0_0CR1BL_MID 0x77 //
-#define TIMER0_0CR1BH_MAX 0x01 // 250000 * 0.002s = 500 = 0x1f4
-#define TIMER0_0CR1BL_MAX 0xf4 //
+#define TIMER1_OCR1A ((SYSTEM_TICKS / PRESCALAR) / TICKS_PER_SECOND)
+/**********************************************************************
+ *
+ * f_cpu/prescalar = 16000000/8 = 2000000 t/s
+ * 2000000 t/s * 0.020  = 39999 = 0x9c3f
+ * 2000000 t/s * 0.001  = 2000  = 0x07d0
+ * 2000000 t/s * 0.0015 = 3000  = 0x0bb8
+ * 2000000 t/s * 0.002  = 3999  = 0x0f9f
+ *
+ **********************************************************************/
+#define TIMER1_0CR1AH 0x9c
+#define TIMER1_0CR1AL 0x3f
+#define TIMER1_0CR1BH_MID 0x07
+#define TIMER1_0CR1BL_MID 0xd0
+// #define TIMER1_0CR1BH_MIN 0x00 // 250000 * 0.001s = 250 = 0xfa
+// #define TIMER1_0CR1BL_MIN 0xfa //
+// #define TIMER1_0CR1BH_MAX 0x01 // 250000 * 0.002s = 500 = 0x1f4
+// #define TIMER1_0CR1BL_MAX 0xf4 //
 
 /* Estructura de datos del driver TIMER */
 typedef struct
@@ -41,8 +51,7 @@ typedef struct
         uint8_t control_reg_a;      // TCC1RA
         uint8_t control_reg_b;      // TCC1RB
         uint8_t control_reg_c;      // TCC1RC
-        uint8_t _reserved1;         //
-        uint8_t _reserved2;         //
+        uint8_t _reserved;          //
         uint8_t counter_reg_l;      // TCNT1L
         uint8_t counter_reg_h;      // TCNT1H
         uint8_t in_capture_reg_l;   // ICR1L
@@ -54,16 +63,18 @@ typedef struct
 } volatile timer1_t;
 volatile timer1_t *timer = (timer1_t *)0x80; // Direccion base
 
-volatile uint8_t *timer_interrupt_mask_reg = (uint8_t *)0x6f; // TIMSK (no se si sirve de algo)
-volatile uint8_t *timer_interrupt_flag_reg = (uint8_t *)0x36; // TIFR1 (no se si sirve de algo)
-volatile uint8_t *input_capture_register_1h = (uint8_t *)0x87; // ICR1H 
-volatile uint8_t *input_capture_register_1l = (uint8_t *)0x86; // ICR1L 
+volatile uint8_t *timer_interrupt_mask_reg = (uint8_t *)0x6f;  // TIMSK (no se si sirve de algo)
+volatile uint8_t *timer_interrupt_flag_reg = (uint8_t *)0x36;  // TIFR1 (no se si sirve de algo)
+volatile uint8_t *input_capture_register_1h = (uint8_t *)0x87; // ICR1H
+volatile uint8_t *input_capture_register_1l = (uint8_t *)0x86; // ICR1L
 
 volatile int ticks;
-extern volatile int task_sync;
 
 int timer1_init(char mode)
 {
+        timer->counter_reg_l = 0;
+        timer->counter_reg_h = 0;
+
         if (mode == 1)
         {
                 timer->control_reg_a |= CONF_CONTROL_REG_A_FPWM;
@@ -81,15 +92,16 @@ int timer1_init(char mode)
                 return 1;
         }
         /* 0CR1A detemines the frequency */
-        timer->out_compare_reg_ah = TIMER0_0CR1AH;
-        timer->out_compare_reg_al = TIMER0_0CR1AL;
-        // (*input_capture_register_1h) = TIMER0_0CR1AH;
-        // (*input_capture_register_1l) = TIMER0_0CR1AL;
+        // timer->out_compare_reg_ah = TIMER0_0CR1AH;
+        // timer->out_compare_reg_al = TIMER0_0CR1AL;
+
         /* 0CR1B detemines the width of high signal in each cycle */
-        timer->out_compare_reg_bh = TIMER0_0CR1BH_MID;
-        timer->out_compare_reg_bl = TIMER0_0CR1BL_MID;
         // *(timer_interrupt_mask_reg) |= CONF_MASK_COMP_A;
-        
+        (*input_capture_register_1l) = TIMER1_0CR1AL;
+        (*input_capture_register_1h) = TIMER1_0CR1AH;
+        timer->out_compare_reg_al = TIMER1_0CR1BL_MID;
+        timer->out_compare_reg_ah = TIMER1_0CR1BH_MID;
+
         ticks = 0;
         return 0;
 }
