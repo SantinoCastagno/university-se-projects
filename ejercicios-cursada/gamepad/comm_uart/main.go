654fb8afb68ae64fddb3caebadf46d2d64d3ce87
Santino Castagno <santinocastagno.dev@gmail.com>
2023-07-18 23:47:55 -0300

    gamepad finished and tested

--- ejercicios-cursada/gamepad/comm_uart/main.go
+++ ejercicios-cursada/gamepad/comm_uart/main.go
@@ -30,45 +30,56 @@ func main() {
 			panic(err)
 		}
 		valor = valor_lectura[0]
-		switch valor {
-		case 48:
-			if keypress != 0 {
-				exec.Command("xdotool", "keyup", "a").Run()
-				exec.Command("xdotool", "keyup", "d").Run()
-				keypress = 0
-			}
-		case 49:
-			if keypress != 1 {
-				exec.Command("xdotool", "keydown", "-d", "0", "a").Run()
-				keypress = 1
-			}
-		case 50:
-			if keypress != 2 {
-				exec.Command("xdotool", "keydown", "-d", "0", "d").Run()
-				keypress = 2
-			}
-		case 51:
-			exec.Command("xdotool", "key", "-d", "0", "f").Run()
-			exec.Command("xdotool", "keydown", "-d", "0", "a").Run()
-		case 52:
-			exec.Command("xdotool", "key", "-d", "0", "f").Run()
-			exec.Command("xdotool", "keydown", "-d", "0", "d").Run()
-		case 53:
-			if keypress != 0 {
-				exec.Command("xdotool", "keyup", "a").Run()
-				exec.Command("xdotool", "keyup", "d").Run()
-				keypress = 0
-			}
-			exec.Command("xdotool", "key", "-d", "0", "f").Run()
-		default:
-			fmt.Println("Invalid option")
-		}
-
 		defer func() {
 			if err := fi.Close(); err != nil {
 				panic(err)
 			}
 		}()
+		switch valor {
+		case 48:
+			if keypress != 0 {
+				exec.Command("xdotool", "keyup", "a").Run()
+				exec.Command("xdotool", "keyup", "d").Run()
+				exec.Command("xdotool", "keyup", "f").Run()
+				keypress = 0
+			}
+		case 49:
+			if keypress != 1 {
+				exec.Command("xdotool", "keyup", "d").Run()
+				exec.Command("xdotool", "keyup", "f").Run()
+				exec.Command("xdotool", "keydown", "a").Run()
+				keypress = 1
+			}
+		case 50:
+			if keypress != 2 {
+				exec.Command("xdotool", "keyup", "a").Run()
+				exec.Command("xdotool", "keyup", "f").Run()
+				exec.Command("xdotool", "keydown", "d").Run()
+				keypress = 2
+			}
+		case 51:
+			if keypress != 3 {
+				exec.Command("xdotool", "keyup", "d").Run()
+				exec.Command("xdotool", "keydown", "a+f").Run()
+				keypress = 3
+			}
+		case 52:
+			if keypress != 4 {
+				exec.Command("xdotool", "keyup", "a").Run()
+				exec.Command("xdotool", "keydown", "d+f").Run()
+				keypress = 4
+			}
+		case 53:
+			if keypress != 5 {
+				exec.Command("xdotool", "keyup", "a").Run()
+				exec.Command("xdotool", "keyup", "d").Run()
+				exec.Command("xdotool", "keydown", "f").Run()
+				keypress = 5
+			}
+		default:
+			fmt.Println("Invalid option")
+		}
+		time.Sleep(5 * time.Millisecond)
 		// time.Sleep(100 * time.Millisecond)
 	}
 }
